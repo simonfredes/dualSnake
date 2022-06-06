3844d703642bbe91a1f29cf4413fadfe62717062
Ramiro Cejas <84473095+ramiro-cejas@users.noreply.github.com>
2022-06-06 15:34:42 -0300

    Update sketch_220606b.js

--- sketch_220606b/sketch_220606b.js
+++ sketch_220606b/sketch_220606b.js
@@ -8,6 +8,7 @@ function setup() {
   s= new Snake();  
   f = new food();
   f.generate();
+  c = new collision();
 }
 
 
@@ -16,6 +17,20 @@ function draw() {
   s.update();
   s.show();
   f.show();
+  c.detect();
+}
+
+function collision(){
+  this.detect = function(){
+    console.log("Entre");
+    if (s.getX() < f.getX() + this.ancho &&
+      s.getX() + this.ancho > f.getX() &&
+      s.getY() < f.getY() + this.alto &&
+      this.alto + s.getY() > f.getY()) {
+        console.log("Detecte");
+        f.generate();
+     }
+  }
 }
 
 function keyPressed(){
@@ -27,6 +42,7 @@ function keyPressed(){
      case 'ArrowLeft' : s.dir(-1,0);break;
     }
 }
+
 function food(){
   this.fX=0;
   this.fY=0;
@@ -36,10 +52,17 @@ function food(){
     this.fY= random(height);
   }
   
+  this.getX = function(){
+    return this.fX;
+  }  
+  
+  this.getY = function(){
+    return this.fY;
+  }
+  
   this.show = function () {
     fill(250,0,0);
-    rect(this.fX, this.fY , ancho, alto);
-
+    rect(this.fX, this.fY , alto, ancho);
   }
 }
 
@@ -48,23 +71,39 @@ function Snake() {
   this.y=0;
   this.speedx=1;  
   this.speedy=0;
+  this.longitud=500;
+  this.trail = [];
+  this.i = 0;
+  
+  this.getX = function(){
+    return this.x;
+  }  
+  this.getY = function(){
+    return this.y;
+  }
   
   this.update= function(){
     this.x= this.x + this.speedx;
     this.y= this.y + this.speedy;
+    this.i = this.i++;
+    this.trail[this.trail.length] = this.x;
+    this.trail[this.trail.length+1] = this.y;
   }
   
   this.show = function () {
     fill(255);
     rect(this.x, this.y, ancho, alto); 
+    for(let c = this.longitud ; c>0; c--){
+      let coorX = this.trail[this.trail.length-c*2];
+      let coorY = this.trail[this.trail.length-c*2-1];
+      fill(255);
+      rect(coorX,coorY,ancho,alto);
+    }
   }
-
+  
   this.dir = function(x,y){
    this.speedx= x;
    this.speedy= y;
   }
 
-  
-
-  
 }
